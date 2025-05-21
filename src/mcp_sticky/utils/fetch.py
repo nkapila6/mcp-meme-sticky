947f5cc6b207fb96d86b51dda1f369a3dfe62cad
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2025-05-22 00:40:21 +0400

    feat(fetch.py): added new embedder files.

--- src/mcp_sticky/utils/fetch.py
+++ src/mcp_sticky/utils/fetch.py
@@ -10,8 +10,61 @@ Created on 2025-05-18 23:20:31 Sunday
 import pickle
 from .crawlers.google import GoogleCrawler
 
+from mediapipe.tasks import python
+from mediapipe.tasks.python import text
+
 TELEGRAM_BOT_URL = "https://t.me/mcp_sticky_bot?text="
 MEMEGEN_CUSTOM_URL = "https://api.memegen.link/images/custom/_/{text}.png?background={url}"
+
+def fetch_embedder(path:str, l2_normalize:bool=True, quantize:bool=True):# ->text.text_embedder.TextEmbedder:
+    base_options = python.BaseOptions(model_asset_path=path)
+    l2_normalize, quantize = True, True
+    options = text.TextEmbedderOptions(
+        base_options=base_options, l2_normalize=l2_normalize, quantize=quantize)
+    embedder = text.TextEmbedder.create_from_options(options)
+    return embedder
+
+def fetch_key(query:str, embedder_path:str, embedding_db_path:str)->str:
+    """Fetch a key from the meme template.
+
+    Args:
+        query (str): Description/Context of meme from MCP host.
+        embedder_path (str): Path of embedder.
+        embedding_db_path (str): Path of embedding db.
+
+    Returns:
+        str: Key in embedding db.
+    """
+    embedder = fetch_embedder(embedder_path)
+    embedding_db = fetch_resource(embedding_db_path)
+    query_embedding = embedder.embed(query).embeddings[0]
+
+    scores = {}
+    for k,template_embedding in embedding_db.items():
+        scores[k] = text.TextEmbedder.cosine_similarity(
+                        template_embedding, query_embedding)
+
+    filter_scores = {k:v for k,v in scores.items() if v>0.95}
+
+    if len(filter_scores)>0: # if length of filter scores is>0 then pick one
+        return pick_random_key(list(filter_scores.keys()))
+    else: # if filter scores has no match then lower threshold and check
+        filter_scores = {k:v for k,v in scores.items() if v>0.80}
+        if len(filter_scores)>0: return pick_random_key(list(filter_scores.keys()))
+        else: return pick_random_key(list(scores.keys())) 
+        
+def pick_random_key(keys:list)->str:
+    """Pick random key.
+
+    Args:
+        keys (list): List of keys.
+
+    Returns:
+        str: Chosen random key.
+    """
+    import random
+    return random.choice(keys)
+    
 
 def fetch_resource(path:str)->dict:
     """Fetches a pickled object at specified path.
